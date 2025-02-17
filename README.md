8033b80045fddbbf7fb759f7dccfe4431c981f47
Patrick Hennig <patrick_hennig@fastmail.com>
2025-02-17 09:47:32 +0000

    added instructions to build realtime kernel for the rpi5

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ files:
 - _tmux.conf_: basic [tmux](https://github.com/tmux/tmux) config file to enable mouse input
 - _virtualbox.md_: some notes on virtualbox usage.
 - _wezterm.lua_: basic [wezterm](https://wezfurlong.org/wezterm/index.html) terminal emulator config file that just increases font size.
+- _rpi5-build-linux6.13rt.sh_: build script to compile the 6.13 linux kernel with realtime scheduling enabled for the rasberry pi 5 (rpi5)
 
 ## python
 
---
+++ rpi5-build-linux6.13rt.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+# adapted from https://forum.linuxcnc.org/9-installing-linuxcnc/55048-raspberry-pi-os-preempt-rt-6-13-kernel-cookbook
+# this script here does NOT check the signature of the patch file
+
+# another tip from that link is to change the WM protocol from wayland to X11
+# (with raspi-config in display settings)
+# because x11 has lower latency
+
+mkdir linux-6.13-rt
+cd linux-6.13-rt
+
+git clone -b rpi-6.13.y --depth=1 https://github.com/raspberrypi/linux
+
+sudo apt install bc bison flex libssl-dev make
+sudo apt install libncurses5-dev
+
+# change from the post: non-rc patch
+curl -OL https://cdn.kernel.org/pub/linux/kernel/projects/rt/6.13/patch-6.13-rt5.patch.gz
+gzip -d patch-6.13-rt5.patch.gz
+
+# cd into the repo
+cd linux
+
+patch -p1 < ../patch-6.13-rc6-rt3.patch
+
+echo "KERNEL=kernel_2712" > .config
+make bcm2712_defconfig
+
+# change config manually (see post!)
+# not actually run here because RT is enabled without it, and starting an interactive tool from within a script is bad style
+# $ make menuconfig
+
+# then manually change one line in .config (slightly adapted from the post for the rpi5, the 16k is for 16k pages):
+# from: CONFIG_LOCALVERSION="-v8-16k"
+# to:   CONFIG_LOCALVERSION="-v8_full_preempt-16k"
+# that is just for display purposes, so not required.
+
+# this takes probably less than the 2h10m for the rpi4 on the more powerful rpi5, but i have not timed it
+# also not sure why 6 tasks on 4core machines, but whatever..
+make -j6 Image.gz modules dtbs 2>&1 | tee make.log
+sudo make -j6 modules_install 2>&1 | tee install.log
+
+sudo cp /boot/firmware/kernel_2712.img /boot/firmware/kernel_2712-backup.img
+sudo cp arch/arm64/boot/Image.gz /boot/firmware/kernel_2712.img
+sudo cp arch/arm64/boot/dts/broadcom/*.dtb /boot/firmware/
+sudo cp arch/arm64/boot/dts/overlays/*.dtb* /boot/firmware/overlays/
+sudo cp arch/arm64/boot/dts/overlays/README /boot/firmware/overlays/
+
+echo "add the kernel=kernel_2712 line /boot/firmware/config.txt (or change if there already is a kernel= line)"
+
+echo "done. reboot to apply."
