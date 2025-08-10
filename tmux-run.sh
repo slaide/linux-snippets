21ec0e8ad4b88b7c0cd75992fd7f4b98f04721d7
Patrick Hennig <patrick_hennig@fastmail.com>
2025-08-10 14:03:06 +0200

    update tmux util script

--- tmux-run.sh
+++ tmux-run.sh
@@ -2,22 +2,42 @@
 
 # helper script to display tmux shortcuts in open tab when opening tmux (via this script)
 
-# open new session called "main"
-tmux new-session -d -s main -c .
+SESSION_NAME=${1:-main}
 
-tmux new-window -t main -c ~ 'echo "
+if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
+    tmux attach-session -t "$SESSION_NAME"
+else
+    tmux new-session -d -s "$SESSION_NAME" -c ~
+    tmux new-window -t "$SESSION_NAME" -c ~ 'echo "
 tmux cheat sheet:
-C-b %       vertical split (side by side)
-C-b \"      horizontal split (top/bottom)
-C-b o       switch between panes
-C-b arrow   navigate panes with arrows
-C-b x       close pane
-C-b c       new window/tab
-C-b n       next window
-C-b p       previous window
-C-b ,       rename window
-C-b ?       full help
-C-b d       detach session
+C-b %        vertical split (side by side)
+C-b \"       horizontal split (top/bottom)
+C-b o        switch between panes
+C-b arrow    navigate panes with arrows
+C-b x        close pane
+C-b c        new window/tab
+C-b n        next window
+C-b p        previous window
+C-b ,        rename window
+C-b s        list/switch sessions
+C-b d        detach from session
+C-b $        rename current session
+C-b ?        full help
+
+C-b : commands (type after C-b :):
+new-session                            create new session
+new-window                             create new window
+rename-window                          rename current window
+select-window                          switch to window by number
+attach-session                         attach to session
+kill-session                           kill current session
+list-sessions                          show all sessions
+source-file ~/.tmux.conf               reload config
+resize-pane -L 5                       resize pane left 5 cells
+resize-pane -R 5                       resize pane right 5 cells
+set-window-option automatic-rename off keep custom window names
 " | less'
-tmux select-window -t main:0
-tmux attach-session -t main
+    tmux rename-window -t "$SESSION_NAME":1 "help"
+    tmux select-window -t "$SESSION_NAME":0
+    tmux attach-session -t "$SESSION_NAME"
+fi
